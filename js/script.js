27fcbb457b4674fdbacec6a2b29c20650a2dd1e5
Roshun Sunder <roshun.sunder@gmail.com>
2022-01-02 17:00:43 -0800

    syncing offline work to repo

---
+++ js/script.js
@@ -0,0 +1,343 @@
+/**
+ * WEBSITE: https://themefisher.com
+ * TWITTER: https://twitter.com/themefisher
+ * FACEBOOK: https://www.facebook.com/themefisher
+ * GITHUB: https://github.com/themefisher/
+ */
+
+
+/* ########################################### Particle Script ############################################## */
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particlesArray;
+
+//get mouse positions
+let mouse = {
+    x: null,
+    y: null,
+    radius: (canvas.height/80) * (canvas.width/80)
+}
+
+window.addEventListener('mousemove',
+    function(event) {
+        mouse.x = event.x;
+        mouse.y = event.y;
+    }
+);
+
+
+//create particle
+class Particle {
+    constructor(x, y, directionX, directionY, size, color) {
+        this.x = x;
+        this.y = y;
+        this.directionX = directionX;
+        this.directionY = directionY;
+        this.size = size;
+        this.color = color;
+    }
+
+    //method to draw individual particles
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
+        ctx.fillStyle = '#83677B';
+        ctx.fill();
+    }
+    //check particle position, check mouse position, move the particle, draw the particle
+    update() {
+        //check if particle is still within canvas
+        if (this.x > canvas.width || this.x < 0) {
+            this.directionX = -this.directionX;
+        }
+
+        if (this.y > canvas.height || this.y < 0) {
+            this.directionY = -this.directionY;
+        }
+
+        //check collision detection - mouse position / particle position
+        let dx = mouse.x - this.x;
+        let dy = mouse.y - this.y; 
+        let distance = Math.sqrt(dx*dx + dy*dy);
+        if (distance < mouse.radius + this.size) {
+            if (mouse.x < this.x && this.x < canvas.width - this.size * 10) {
+                this.x += 10;
+            }
+            if (mouse.x > this.x && this.x > this.size * 10) {
+                this.x -= 10;
+            }
+            if (mouse.y < this.y && this.y < canvas.height - this.size * 10) {
+                this.y += 10;
+            }
+            if (mouse.y > this.y && this.y > this.size * 10) {
+                this.y -= 10;
+            }
+        }
+        //move particle
+        this.x += this.directionX;
+        this.y += this.directionY;
+        //draw particle
+        this.draw();
+    }
+}
+//create particle array
+function init() {
+    particlesArray = [];
+    let numberOfParticles = (canvas.height * canvas.width) / 9000;
+    for (let i = 0; i < numberOfParticles; i++) {
+        let size = (Math.random() * 5) + 1;
+        let x = (Math.random() * ((innerWidth - size * 2) - (size * 2)) + size * 2);
+        let y = (Math.random() * ((innerHeight - size * 2) - (size * 2)) + size * 2);
+        let directionX = (Math.random() * 5) - 2.5;
+        let directionY = (Math.random() * 5) - 2.5;
+        let color = '#83677B';
+
+        particlesArray.push(new Particle(x, y, directionX, directionY, size, color));
+    }
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    ctx.clearRect(0,0, innerWidth, innerHeight);
+
+    for (let i = 0; i < particlesArray.length; i++) {
+        particlesArray[i].update();
+    }
+
+    connect();
+}
+
+function connect() {
+    let opactiyValue = 1;
+    for (let a = 0; a < particlesArray.length ; a++) {
+        for (let b = a; b < particlesArray.length; b++) {
+            let distance = ((particlesArray[a].x - particlesArray[b].x) * (particlesArray[a].x - particlesArray[b].x)) + ((particlesArray[a].y - particlesArray[b].y) * (particlesArray[a].y - particlesArray[b].y));
+            if (distance < (canvas.width/7) * (canvas.height/7)) {
+                opactiyValue = 1 - (distance/20000);
+                ctx.strokeStyle = 'rgba(131,103,123,' + opactiyValue + ')';
+                ctx.lineWidth = 1;
+                ctx.beginPath();
+                ctx.moveTo(particlesArray[a].x, particlesArray[a].y);
+                ctx.lineTo(particlesArray[b].x, particlesArray[b].y);
+                ctx.stroke();
+            }
+        }
+    }
+}
+
+window.addEventListener('resize',
+    function() {
+        canvas.width = innerWidth;
+        canvas.height = innerHeight;
+        mouse.radius = ((canvas.height/80) * (canvas.height/80));
+        init();
+    
+    }
+);
+
+window.addEventListener('mouseout',
+    function() {
+        mouse.x = undefined;
+        mouse.y = undefined;
+    }
+);
+
+//if mouse stops moving
+var timeout;
+document.onmousemove = function(){
+  clearTimeout(timeout);
+  timeout = setTimeout(function(){mouse.x = undefined; mouse.y = undefined}, 2);
+}
+
+init();
+animate();
+
+/* ########################################### /Particle Script ############################################## */
+
+/* #### Fade-In JS #### */
+const faders = document.querySelectorAll(".fade-in");
+
+const appearOptions = {
+    threshold: 0.5,
+    rootMargin: "0px 0px -100px 0px"
+  };
+
+const appearOnScroll = new IntersectionObserver(function(
+    entries,
+    appearOnScroll
+  ) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      } else {
+        entry.target.classList.add("appear");
+        appearOnScroll.unobserve(entry.target);
+      }
+    });
+  },
+  appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+});
+
+(function ($) {
+    'use strict';
+
+
+
+    // Sticky Menu
+    $(window).scroll(function () {
+        if ($('.navigation').offset().top > 100) {
+            $('.navigation').addClass('nav-bg');
+        } else {
+            $('.navigation').removeClass('nav-bg');
+        }
+    });
+
+    // Background-images
+    $('[data-background]').each(function () {
+        $(this).css({
+            'background-image': 'url(' + $(this).data('background') + ')'
+        });
+    });
+
+    // background color
+    $('[data-color]').each(function () {
+        $(this).css({
+            'background-color': $(this).data('color')
+        });
+    });
+
+    // progress bar
+    $('[data-progress]').each(function () {
+        $(this).css({
+            'bottom': $(this).data('progress')
+        });
+    });
+
+
+    /* ########################################### hero parallax ############################################## */
+    window.onload = function () {
+
+        var parallaxBox = document.getElementById('parallax');
+        var
+            /* c1left = document.getElementById('l1').offsetLeft,
+                       c1top = document.getElementById('l1').offsetTop, */
+            c2left = document.getElementById('l2').offsetLeft,
+            c2top = document.getElementById('l2').offsetTop,
+            c3left = document.getElementById('l3').offsetLeft,
+            c3top = document.getElementById('l3').offsetTop,
+            c4left = document.getElementById('l4').offsetLeft,
+            c4top = document.getElementById('l4').offsetTop,
+            c5left = document.getElementById('l5').offsetLeft,
+            c5top = document.getElementById('l5').offsetTop,
+            c6left = document.getElementById('l6').offsetLeft,
+            c6top = document.getElementById('l6').offsetTop,
+            c7left = document.getElementById('l7').offsetLeft,
+            c7top = document.getElementById('l7').offsetTop,
+            c8left = document.getElementById('l8').offsetLeft,
+            c8top = document.getElementById('l8').offsetTop,
+            c9left = document.getElementById('l9').offsetLeft,
+            c9top = document.getElementById('l9').offsetTop;
+
+        parallaxBox.onmousemove = function (event) {
+            event = event || window.event;
+            var x = event.clientX - parallaxBox.offsetLeft,
+                y = event.clientY - parallaxBox.offsetTop;
+
+            /*  mouseParallax('l1', c1left, c1top, x, y, 5); */
+            mouseParallax('l2', c2left, c2top, x, y, 25);
+            mouseParallax('l3', c3left, c3top, x, y, 20);
+            mouseParallax('l4', c4left, c4top, x, y, 35);
+            mouseParallax('l5', c5left, c5top, x, y, 30);
+            mouseParallax('l6', c6left, c6top, x, y, 45);
+            mouseParallax('l7', c7left, c7top, x, y, 30);
+            mouseParallax('l8', c8left, c8top, x, y, 25);
+            mouseParallax('l9', c9left, c9top, x, y, 40);
+        };
+
+    };
+
+    function mouseParallax(id, left, top, mouseX, mouseY, speed) {
+        var obj = document.getElementById(id);
+        var parentObj = obj.parentNode,
+            containerWidth = parseInt(parentObj.offsetWidth),
+            containerHeight = parseInt(parentObj.offsetHeight);
+        obj.style.left = left - (((mouseX - (parseInt(obj.offsetWidth) / 2 + left)) / containerWidth) * speed) + 'px';
+        obj.style.top = top - (((mouseY - (parseInt(obj.offsetHeight) / 2 + top)) / containerHeight) * speed) + 'px';
+    }
+    /* ########################################### /hero parallax ############################################## */
+
+    // testimonial-slider
+    $('.testimonial-slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        arrows: false,
+        adaptiveHeight: true
+    });
+
+
+    // clients logo slider
+    $('.client-logo-slider').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        dots: false,
+        arrows: false,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 400,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+    // Shuffle js filter and masonry
+    var Shuffle = window.Shuffle;
+    var jQuery = window.jQuery;
+
+    var myShuffle = new Shuffle(document.querySelector('.shuffle-wrapper'), {
+        itemSelector: '.shuffle-item',
+        buffer: 1
+    });
+
+    jQuery('input[name="shuffle-filter"]').on('change', function (evt) {
+        var input = evt.currentTarget;
+        if (input.checked) {
+            myShuffle.filter(input.value);
+        }
+    });
+
+
+
+})(jQuery);
