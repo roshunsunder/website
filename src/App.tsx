9da7428c410431353b01dbcc1ee8b618fbbd7adb
roshunsunder <roshun.sunder@gmail.com>
2026-01-28 13:53:59 -0500

    Add star background

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Canvas } from '@react-three/fiber'
-import { Environment, OrbitControls, useGLTF } from '@react-three/drei'
+import { Environment, OrbitControls, Stars, useGLTF } from '@react-three/drei'
 import './App.css'
 
 function Earth() {
@@ -11,6 +11,7 @@ function App() {
   return (
     <div className="canvas-container">
       <Canvas camera={{ position: [0, 0, 75], fov: 50 }} gl={{ toneMappingExposure: 0.5 }}>
+        <Stars radius={300} depth={60} count={5000} factor={4} saturation={0} fade speed={0} />
         <Environment preset="studio" background={false} />
         <Earth />
         <OrbitControls enableZoom={true} enablePan={true} enableRotate={true} />
